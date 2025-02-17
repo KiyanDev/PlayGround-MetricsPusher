f2302e6344ddca011a18a2fdf914ec6e7ff4b3e5
Gabriel de Lamo <gabriel.delamo@phios.group>
2025-02-17 16:35:46 +0100

    include hosted service and a lot of packages, include the otel-collector hardcoded

--- PlayGround-MetricsPusher/Program.cs
+++ PlayGround-MetricsPusher/Program.cs
@@ -1,11 +1,51 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+using PlayGround_MetricsPusher;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string otelEndpoint = "http://otel-collector:4317"; // Uses the K8s service name
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder => tracerProviderBuilder
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("PlaygroundMetricsPusher"))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(otelEndpoint);
+            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        }))
+    .WithMetrics(metricsProviderBuilder => metricsProviderBuilder
+        .AddMeter("PlaygroundMetricsPusher")
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(otelEndpoint);
+            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        }));
+
+builder.Logging.AddOpenTelemetry(loggingOptions =>
+{
+    loggingOptions.IncludeFormattedMessage = true;
+    loggingOptions.IncludeScopes = true;
+    loggingOptions.ParseStateValues = true;
+    loggingOptions.AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri(otelEndpoint);
+        options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+    });
+});
+
+builder.Services.AddHostedService<TelemetryWorker>();
 
 var app = builder.Build();
 
---
+++ PlayGround-MetricsPusher/TelemetryWorker.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace PlayGround_MetricsPusher;
+
+public class TelemetryWorker : BackgroundService
+{
+    private readonly ILogger<TelemetryWorker> _logger;
+    private readonly Meter _meter;
+    private readonly Counter<long> _metricCounter;
+
+    public TelemetryWorker(ILogger<TelemetryWorker> logger)
+    {
+        _logger = logger;
+        _meter = new Meter("PlaygroundMetricsPusher");
+        _metricCounter = _meter.CreateCounter<long>("custom_metric_counter");
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Sending telemetry at: {Time}", DateTimeOffset.Now);
+
+            // Emit a metric
+            _metricCounter.Add(1, new KeyValuePair<string, object?>("status", "ok"));
+
+            await Task.Delay(1000, stoppingToken); // Run every second
+        }
+    }
+}
